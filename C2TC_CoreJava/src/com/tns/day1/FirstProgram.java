30d4cebc46a994042588e65332f7dd5237e0ff67
JJJ <JJJ@DESKTOP-KSB80V9>
2024-09-04 11:51:48 +0530

    this is my commit

---
+++ C2TC_CoreJava/src/com/tns/day1/FirstProgram.java
@@ -0,0 +1,31 @@
+package com.tns.day1;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a=100;
+	int b=90;
+	System.out.println("a+b="+(a+b));
+	System.out.println("a-b="+(a-b));
+	System.out.println("a/b="+(a/b));
+	System.out.println("a*b="+(a*b));
+	System.out.println("a%b="+(a%b));
+	System.out.println("a++="+a);
+	System.out.println("a--="+a);
+	System.out.println("a<b="+(a<b));
+	System.out.println("a>=b="+(a>=b));
+	System.out.println("a<=b="+(a<=b));
+	System.out.println("a<b="+(a<b));
+	System.out.println("a==b="+(a==b));
+	System.out.println("a=b="+(a=b));
+	System.out.println("a+=b="+(a+=b));
+	System.out.println("a-=b="+(a-=b));
+	System.out.println("a*=b="+(a*=b));
+	System.out.println("a/=b="+(a/=b));
+	System.out.println("a%=b="+(a%=b));
+	System.out.println((a>b)?"hi":"hello");
+
+	}
+
+}
